32200054d70583188a8ecb7fafc1d1b271063ed4
Shlok Mishra <99207534+Jougan-0@users.noreply.github.com>
2024-06-10 02:10:19 +0530

    Update generate.go
    Signed-off-by: Shlok Mishra <99207534+Jougan-0@users.noreply.github.com>

--- mesheryctl/internal/cli/root/registry/generate.go
+++ mesheryctl/internal/cli/root/registry/generate.go
@@ -55,7 +55,7 @@ var (
 )
 var (
 	artifactHubCount        = 0
-	artifactHubRateLimit    = 120
+	artifactHubRateLimit    = 110
 	artifactHubRateLimitDur = 5 * time.Minute
 	artifactHubMutex        sync.Mutex
 )
